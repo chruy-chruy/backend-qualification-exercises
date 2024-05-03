c1bb7e7e402a6db3b757eb88e8bf6eb349f30d3a
Yourname <you@example.com>
2024-05-03 15:50:46 +0800

    add serialize

--- src/level1/problem1/index.ts
+++ src/level1/problem1/index.ts
@@ -7,11 +7,51 @@ export type Value = string | number | boolean | null | undefined |
  * compatible objects.
  */
 export function serialize(value: Value): unknown {
-  /**
-   * insert your code here
-   */
+  if (typeof value == "string" || typeof value == "number" || typeof value == "boolean" || value == null || value == undefined) {
+    return value;
+  }
   
-  return;
+  else if(value instanceof Map){
+    const mapToArray = Array.from(value.entries());
+    const obj = 
+    {
+      "__t": "Map",
+      "__v": mapToArray
+    }; 
+
+    return obj;
+  }
+  else if(value instanceof Set){
+    const setToArray = Array.from(value);
+    const obj = 
+    {
+      "__t": "Set",
+      "__v": setToArray
+    }; 
+
+    return obj;
+  }
+  else if(value instanceof Buffer){
+    const bufferToArray = Array.from(value);
+    const obj = 
+    {
+      "__t": "Buffer",
+      "__v": bufferToArray
+    }; 
+
+    return obj;
+  }
+  else if(value instanceof Date){
+    const dateToTimestamp =  value.getTime()
+    const obj = 
+    {
+      "__t": "Date",
+      "__v": dateToTimestamp
+    }; 
+
+    return obj;
+  }
+  
 }
 
 /**
@@ -19,9 +59,12 @@ export function serialize(value: Value): unknown {
  * scalar and objects.
  */
 export function deserialize<T = unknown>(value: unknown): T {
-  /**
-   * insert your code here
-   */
+  if (typeof value == "string" || typeof value == "number" || typeof value == "boolean" || value == null || value == undefined) {
+    return value as T;
+  }
+  console.log(value)
+  
+
   
   return;
 }
